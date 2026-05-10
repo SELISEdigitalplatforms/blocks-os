b53e78dded13f776790ed20b048ed723c9bb073f
afiapreety <afia.preety@selisegroup.com>
2026-05-10 19:24:11 +0600

    fix: back to localstorage + Userchecker

--- client/app/guards/protected-guard.tsx
+++ client/app/guards/protected-guard.tsx
@@ -2,14 +2,32 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuthStore } from "@/store/useAuthStore";
 import { useAppState } from "./public-guard";
+import { IDP_BASE_URL } from "@/constants/endpoint.constant";
+import { useGetUser } from "@/idp/iam/hooks/use-user";
+
 export function ProtectedGuard({ children }: { children: React.ReactNode }) {
   const { isAuthenticated } = useAuthStore();
   const { isMounted } = useAppState();
   const navigate = useNavigate();
+
   useEffect(() => {
     if (!isMounted) return;
-    if (!isAuthenticated) return navigate("/login", { replace: true });
+    if (!isAuthenticated) return navigate(`${IDP_BASE_URL}/login`, { replace: true });
   }, [isAuthenticated, isMounted, navigate]);
   if (!isMounted || !isAuthenticated) return null;
+  return <>{children}</>;
+}
+
+export function UserChecker({ children }: { children: React.ReactNode }) {
+    const navigate = useNavigate();
+    const { data: loggedInUser, isLoading } = useGetUser();
+    const { setUser } = useAuthStore();
+
+    useEffect(() => {
+        if(!loggedInUser?.data) return navigate(`${IDP_BASE_URL}/login`, { replace: true });
+        setUser(loggedInUser.data);
+    }, [loggedInUser, navigate, setUser]);
+
+    if(isLoading) return null;
   return <>{children}</>;
 }
--- client/app/store/useAuthStore.ts
+++ client/app/store/useAuthStore.ts
@@ -1,6 +1,6 @@
 import { User } from "@blocks-idp/iam/models/user";
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 interface AuthState {
   isAuthenticated: boolean;
@@ -48,18 +48,6 @@ export const useAuthStore = create<AuthState>()(
     }),
     {
       name: "auth-storage",
-      storage: {
-        getItem: (name) => {
-          const value = sessionStorage.getItem(name);
-          return value ? JSON.parse(value) : null;
-        },
-        setItem: (name, value) => {
-          sessionStorage.setItem(name, JSON.stringify(value));
-        },
-        removeItem: (name) => {
-          sessionStorage.removeItem(name);
-        },
-      },
     },
   ),
 );
