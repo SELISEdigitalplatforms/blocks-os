15c0675456acfb1906b87a793804b6943b6a85c7
afiapreety <afia.preety@selisegroup.com>
2026-05-15 19:38:49 +0600

    fix: absolute true

--- client/app/idp/iam/services/role.service.ts
+++ client/app/idp/iam/services/role.service.ts
@@ -13,15 +13,15 @@ import { ROLE_ENDPOINTS } from "../constants/endpoint.constant";
 
 export class RoleService {
   getRoles(payload: GetRolesPayload): Promise<GetRolesResponse> {
-    return http.post(ROLE_ENDPOINTS.GET_ROLES, payload);
+    return http.post(ROLE_ENDPOINTS.GET_ROLES, payload, undefined, { absoluteUrl: true });
   }
 
   getRoleById(payload: IGetRolePayload): Promise<IGetRoleResponse> {
-    return http.get(`${ROLE_ENDPOINTS.GET_ROLE}?projectKey=${payload.projectKey}&id=${payload.id}`);
+    return http.get(`${ROLE_ENDPOINTS.GET_ROLE}?projectKey=${payload.projectKey}&id=${payload.id}`, undefined, { absoluteUrl: true });
   }
 
   addRole(payload: CreateRolePayload): Promise<IRole> {
-    return http.post(ROLE_ENDPOINTS.CREATE_ROLE, payload);
+    return http.post(ROLE_ENDPOINTS.CREATE_ROLE, payload, undefined, { absoluteUrl: true });
   }
 
   updateRole(payload: UpdateRolePayload) {
@@ -29,11 +29,11 @@ export class RoleService {
       errors: unknown;
       isSuccess: boolean;
       itemId: string;
-    }>(ROLE_ENDPOINTS.UPDATE_ROLE, payload);
+    }>(ROLE_ENDPOINTS.UPDATE_ROLE, payload, undefined, { absoluteUrl: true });
   }
 
   setRoles(addSetRolesPayload: SetRoles): Promise<SetRoles> {
-    return http.post<SetRoles>(ROLE_ENDPOINTS.SET_ROLES, { ...addSetRolesPayload });
+    return http.post<SetRoles>(ROLE_ENDPOINTS.SET_ROLES, { ...addSetRolesPayload }, undefined, { absoluteUrl: true });
   }
 }
 
