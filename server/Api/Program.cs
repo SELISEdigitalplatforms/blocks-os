a90049ff9417aaa78b1bc0e08ee2ea4190be2ef9
Koushik Ghosh <koushik.ghosh@selise.ch>
2026-05-17 18:05:42 +0600

    updated

--- server/Api/Program.cs
+++ server/Api/Program.cs
@@ -61,22 +61,22 @@ if (File.Exists(indexHtml))
 
     app.MapFallback(async context =>
     {
-        // var tenantService = context.RequestServices.GetRequiredService<ITenants>();
-        // var dbContext = context.RequestServices.GetRequiredService<IDbContextProvider>();
-        // var host = context.Request.Host.Value;
-        // var tenant = tenantService.GetTenantByApplicationDomain(host);
-        // var database = dbContext.GetDatabase(tenant.TenantId);
-        // var captcheSetting = await (await database.GetCollection<CaptchaConfiguration>("CaptchaConfigurations").FindAsync(Builders<CaptchaConfiguration>.Filter.Eq(mc => mc.IsEnable, true))).FirstOrDefaultAsync();
-        // ApplyFrontendRuntimeSettings(builder.Configuration, wwwrootPath,  tenant.TenantId, captcheSetting.CaptchaKey);
+        var tenantService = context.RequestServices.GetRequiredService<ITenants>();
+        var dbContext = context.RequestServices.GetRequiredService<IDbContextProvider>();
+        var host = context.Request.Host.Value;
+        var tenant = tenantService.GetTenantByApplicationDomain(host);
+        var database = dbContext.GetDatabase(tenant.TenantId);
+        var captcheSetting = await (await database.GetCollection<CaptchaConfiguration>("CaptchaConfigurations").FindAsync(Builders<CaptchaConfiguration>.Filter.Eq(mc => mc.IsEnable, true))).FirstOrDefaultAsync();
+        ApplyFrontendRuntimeSettings(builder.Configuration, wwwrootPath, tenant.TenantId, captcheSetting.CaptchaKey);
 
-        // context.Response.Cookies.Append("x-blocks-key", tenant.TenantId, new CookieOptions
-        // {
-        //     Domain = tenant.CookieDomain,
-        //     HttpOnly = true,
-        //     Secure = true,
-        //     SameSite =  SameSiteMode.None,
-        //     Path = "/"
-        // });
+        //context.Response.Cookies.Append("x-blocks-key", tenant.TenantId, new CookieOptions
+        //{
+        //    Domain = tenant.CookieDomain,
+        //    HttpOnly = true,
+        //    Secure = true,
+        //    SameSite = SameSiteMode.None,
+        //    Path = "/"
+        //});
 
         await context.Response.SendFileAsync(indexHtml);
 
