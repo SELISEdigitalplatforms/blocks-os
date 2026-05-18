42c1ae92c0af820ce6bd51e3f5872bee511cf7a8
Koushik Ghosh <koushik.ghosh@selise.ch>
2026-05-18 12:45:22 +0600

    make endPoint authorize

--- server/Api/Controllers/ProjectController.cs
+++ server/Api/Controllers/ProjectController.cs
@@ -45,7 +45,8 @@ namespace Api.Controllers
 
 
         [HttpGet]
-        [ProtectedEndPoint("blocks-os::get-projects")]
+        //[ProtectedEndPoint("blocks-os::get-projects")]
+        [Authorize]
         public async Task<List<GroupedProjectsDto>> Gets([FromQuery] GetProjectsRequest request)
         {
             return await _projectManagementService.GetAllAsync(request);
@@ -58,8 +59,9 @@ namespace Api.Controllers
             return await _projectManagementService.RestoreProjectAsync(restoreProjectRequest);
         }
 
-        [ProtectedEndPoint("blocks-os::get-project")]
+        //[ProtectedEndPoint("blocks-os::get-project")]
         [HttpGet]
+        [Authorize] 
         public async Task<GetProjectResponse> Get([FromQuery] string projectId)
         {
             if (string.IsNullOrWhiteSpace(projectId))
