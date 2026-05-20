bbe010b053018e54c921ddf409a77f8aa14aadce
UDay <uday.dev.bd@gmail.com>
2026-05-20 20:15:26 +0600

    refactor: remove ImpersonateGuard from DashboardLayout component

--- client/app/layouts/admin-layout.tsx
+++ client/app/layouts/admin-layout.tsx
@@ -1,12 +1,11 @@
 import { Outlet } from "react-router-dom";
 import { DashboardLayoutProvider } from "@/contexts/dashboard-layout-provider";
-import { ImpersonateGuard, ProtectedGuard } from "@/guards/protected-guard";
+import { ProtectedGuard } from "@/guards/protected-guard";
 import { SidebarMenuDesktop } from "@/layouts/sidebar-menu-desktop/sidebar-menu-desktop";
 import { DashboardHeader } from "@/layouts/dashboard-header/dashboard-header";
 export function DashboardLayout() {
   return (
     <ProtectedGuard>
-      {/* <ImpersonateGuard> */}
       <DashboardLayoutProvider isOpen={true} persist>
         <div className="relative flex h-screen overflow-hidden bg-[hsl(var(--surface-app))]">
           <SidebarMenuDesktop />
@@ -18,7 +17,6 @@ export function DashboardLayout() {
           </div>
         </div>
       </DashboardLayoutProvider>
-      {/* </ImpersonateGuard> */}
     </ProtectedGuard>
   );
 }
