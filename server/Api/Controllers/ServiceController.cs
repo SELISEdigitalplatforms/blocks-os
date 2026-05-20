65acef8fcb0e7edf456af9db4174f6fa71f881ac
Koushik Ghosh <koushik.ghosh@selise.ch>
2026-05-20 20:46:27 +0600

    make endPoint authorize

--- server/Api/Controllers/ServiceController.cs
+++ server/Api/Controllers/ServiceController.cs
@@ -2,6 +2,7 @@
 using DomainService.ManagedService.Services;
 using Microsoft.AspNetCore.Mvc;
 using Blocks.Genesis;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Api.Controllers
 {
@@ -16,7 +17,8 @@ namespace Api.Controllers
             _serviceManagement = serviceManagement;
         }
 
-        [ProtectedEndPoint("blocks-os::service::register")  ]
+        // [ProtectedEndPoint("blocks-os::service::register")  ]
+        [Authorize]
         [HttpPost]
         public async Task<IActionResult> Register([FromBody] RegisterServiceRequest request)
         {
@@ -29,7 +31,8 @@ namespace Api.Controllers
             return BadRequest(response);
         }
 
-        [ProtectedEndPoint("blocks-os::service::get-all")]
+        // [ProtectedEndPoint("blocks-os::service::get-all")]
+        [Authorize]
         [HttpPost]
         public async Task<GetAllServiceResponse> GetAll([FromBody] GetAllServiceRequest request)
         {
