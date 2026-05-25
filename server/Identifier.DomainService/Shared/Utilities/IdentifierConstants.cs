3187156694e1472b07abc0558deadef10455f705
abdullahmomen2037 <abdullah.momen@selisegroup.com>
2026-05-26 02:37:47 +0600

    mail template issue resolve

--- server/Identifier.DomainService/Shared/Utilities/IdentifierConstants.cs
+++ server/Identifier.DomainService/Shared/Utilities/IdentifierConstants.cs
@@ -20,11 +20,11 @@ namespace DomainService.Shared
         public const string MigrationTrackerCollectionName = "MigrationTrackers";
         public const string CookieDomainPrefix = "blocksapi.";
 
-        public const string IdentifierQueueName = "blocks_project_listener";
+        public const string IdentifierQueueName = "blocks_idp_identifier_listener";
         public const string DataCleanupQueue = "blocks_data_cleanup_listener";
         public const string LanguageDataMigrationQueue = "blocks_uilm_environment_data_migration_listener";
-        public const string IamQueue = "blocks_iam_listener";
-        public const string MailQueue = "blocks_mail_listener";
+        public const string IamQueue = "blocks_idp_iam_listener";
+        public const string MailQueue = "blocks_email_listener";
         public const string GenericMigrationQueue = "blocks_generic_migration_listener";
         public const string MigrationCompletionTopic = "migration_topic";
         public const string ProjectPeopleInvitationMailPurpose = "project_invitation";
