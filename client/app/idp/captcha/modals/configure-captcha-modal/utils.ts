4e9f1fa1d7b7e830b785e3bd615bcd560f08e588
afiapreety <afia.preety@selisegroup.com>
2026-06-01 18:01:11 +0600

    fix: captcha api no project keys

--- client/app/idp/captcha/modals/configure-captcha-modal/utils.ts
+++ client/app/idp/captcha/modals/configure-captcha-modal/utils.ts
@@ -1,14 +1,12 @@
 import { z } from "zod";
 
 export const ConfigureCaptchaFormSchema = z.object({
-  secretName: z.string().min(1, "Name is required"),
   provider: z.enum(["recaptcha", "hcaptcha"]),
   captchaKey: z.string().min(1, "Site key is required"),
   captchaSecret: z.string().min(1, "Secret key is required"),
   captchaGenerator: z.string().min(1, "Generator type is required"),
 });
 export const ConfigureCaptchaFormDefaultValue = {
-  secretName: "",
   provider: "",
   captchaKey: "",
   captchaSecret: "",
