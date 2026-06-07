d54d60961586ce169736e34f0148c8506bcb3644
afiapreety <afia.preety@selisegroup.com>
2026-06-08 03:22:25 +0600

    fix: OS package chnages 1

--- client/app/main.tsx
+++ client/app/main.tsx
@@ -3,15 +3,31 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { NuqsAdapter } from "nuqs/adapters/react-router/v6";
 import { Toaster } from "./components/ui-kits/toaster/toaster";
+import { TooltipProvider } from "./components/ui-kits/tooltip/tooltip";
 import QueryProvider from "./providers/query-provider";
 import { router } from "./router";
 import "./styles/globals.css";
+import { BlocksAppLayout } from "@seliseblocks/blocks-kit";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryProvider>
       <NuqsAdapter>
-        <RouterProvider router={router} />
-        <Toaster />
+        <TooltipProvider>
+          <BlocksAppLayout
+            config={{
+              userBaseUrlKey: "BLOCKS_IAM_BASE_URL",
+              projectBaseUrlKey: "BLOCKS_LOGIC_BASE_URL",
+              appLogoUrl: {
+                dark: "/blocks-logos/Logo_Black.svg",
+                light: "/blocks-logos/Logo_White.svg",
+              },
+            }}
+          >
+            <RouterProvider router={router} />
+          </BlocksAppLayout>
+          <Toaster />
+        </TooltipProvider>
       </NuqsAdapter>
     </QueryProvider>
   </StrictMode>,
