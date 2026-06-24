9fcfd48b0d45f6aa54e3e810ec29e47d8be1e209
Sk. Zaber Ahmed <zaber.ahmed@selisegroup.com>
2026-06-24 15:23:21 +0600

    fix: build errors

--- client/app/layouts/dashboard-header/dashboard-header.tsx
+++ client/app/layouts/dashboard-header/dashboard-header.tsx
@@ -63,7 +63,7 @@ export function DashboardHeader() {
           <BackToConsoleNavigator />
           <ThemeSwitcher />
           <Notification />
-          <AppSwitcher forwardedTo="dashboard" />
+          <AppSwitcher forwardedTo="/app/dashboard" />
           <UserDropdownMenu />
         </div>
       </header>
