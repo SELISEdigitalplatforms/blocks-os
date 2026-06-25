0c89393cc6a5222f2de17bbe46398260e0c37466
afiapreety <afia.preety@selisegroup.com>
2026-06-25 16:56:46 +0600

    fix: build fail

--- client/app/layouts/dashboard-header/dashboard-header.tsx
+++ client/app/layouts/dashboard-header/dashboard-header.tsx
@@ -63,7 +63,7 @@ export function DashboardHeader() {
           <BackToConsoleNavigator />
           <ThemeSwitcher />
           <Notification />
-          <AppSwitcher forwardedTo="/dashboard" />
+          <AppSwitcher />
           <UserDropdownMenu />
         </div>
       </header>
