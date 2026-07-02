3ccba295b25df087f5137ed74d6565426100b386
Koushik Ghosh <koushik.ghosh@selise.ch>
2026-07-02 15:26:56 +0600

    update applicationDomain configuration endPoint

--- server/Identifier.DomainService/Project/Validators/UpdateProjectRequestValidator.cs
+++ server/Identifier.DomainService/Project/Validators/UpdateProjectRequestValidator.cs
@@ -1,4 +1,4 @@
-﻿using DomainService.Shared;
+using DomainService.Shared;
 using FluentValidation;
 
 namespace DomainService.Projects
@@ -11,22 +11,16 @@ namespace DomainService.Projects
         {
             _repository = repository;
 
-            // Validation for ProjectId
-            RuleFor(x => x.ProjectKey)
-               .Cascade(CascadeMode.Stop)
-               .NotEmpty()
-               .NotNull();
-
             //RuleFor(x => x.ApplicationDomain)
             //    .Cascade(CascadeMode.Stop)
             //    .Must(IdentifierHelper.BeAValidUrl).WithMessage("ApplicationDomain is not in a valid format.")
             //    .MustAsync(IsUniqueDomain).WithMessage("ApplicationDomain must be unique")
             //    .WhenAsync((x, ct) => IsDomainUpdated(x.ApplicationDomain, x.ProjectKey, ct));
 
-            RuleFor(x => x.CustomDomain)
+            RuleFor(x => x.Domain)
                 .Cascade(CascadeMode.Stop)
                 .Must(IdentifierHelper.BeAValidUrl).WithMessage("CustomDomain is not in a valid format.")
-                .When(x => !string.IsNullOrWhiteSpace(x.CustomDomain));
+                .When(x => !string.IsNullOrWhiteSpace(x.Domain));
         }
 
         //private async Task<bool> IsDomainUpdated(string applicationDomain, string projectId, CancellationToken _)
