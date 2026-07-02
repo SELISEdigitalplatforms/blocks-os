81b2739fc8aa8431c798eb5d629476329928deee
Koushik Ghosh <koushik.ghosh@selise.ch>
2026-07-02 15:44:40 +0600

    updated

--- server/Identifier.DomainService/Project/UpdateProjectRequest.cs
+++ server/Identifier.DomainService/Project/UpdateProjectRequest.cs
@@ -1,10 +1,24 @@
 
 namespace DomainService.Projects
 {
-   public class UpdateProjectRequest
+    public class UpdateProjectRequest
     {
-       public string Domain { get; set; } = string.Empty;
-       public string CookieDomain { get; set; } = string.Empty;
-       public bool IsDomainVerified { get; set; }
+        public ApplicationAction Action { get; set; }
+        public Application Application { get; set; }
+        public string? ApplicationDomain { get; set; }
+    }
+
+    public enum ApplicationAction
+    {
+        Add,
+        Edit,
+        Delete
+    }
+
+    public class Application
+    {
+        public string Domain { get; set; }
+        public string CookieDomain { get; set; }
+        public bool IsDomainVerified { get; set; }
     }
 }
--- server/Identifier.DomainService/Project/Validators/UpdateProjectRequestValidator.cs
+++ server/Identifier.DomainService/Project/Validators/UpdateProjectRequestValidator.cs
@@ -5,40 +5,23 @@ namespace DomainService.Projects
 {
     public class UpdateProjectRequestValidator : AbstractValidator<UpdateProjectRequest>
     {
-        private readonly IProjectRepository _repository;
-
-        public UpdateProjectRequestValidator(IProjectRepository repository)
+        public UpdateProjectRequestValidator()
         {
-            _repository = repository;
+            RuleFor(x => x.Action)
+                .IsInEnum().WithMessage("Invalid action. Must be Add, Edit, or Delete.");
 
-            //RuleFor(x => x.ApplicationDomain)
-            //    .Cascade(CascadeMode.Stop)
-            //    .Must(IdentifierHelper.BeAValidUrl).WithMessage("ApplicationDomain is not in a valid format.")
-            //    .MustAsync(IsUniqueDomain).WithMessage("ApplicationDomain must be unique")
-            //    .WhenAsync((x, ct) => IsDomainUpdated(x.ApplicationDomain, x.ProjectKey, ct));
+            RuleFor(x => x.Application)
+                .NotNull().WithMessage("Application is required.")
+                .When(x => x.Action == ApplicationAction.Add || x.Action == ApplicationAction.Edit);
 
-            RuleFor(x => x.Domain)
-                .Cascade(CascadeMode.Stop)
-                .Must(IdentifierHelper.BeAValidUrl).WithMessage("CustomDomain is not in a valid format.")
-                .When(x => !string.IsNullOrWhiteSpace(x.Domain));
-        }
+            RuleFor(x => x.Application.Domain)
+                .NotEmpty().WithMessage("Domain is required.")
+                .Must(IdentifierHelper.BeAValidUrl).WithMessage("Domain is not in a valid format.")
+                .When(x => (x.Action == ApplicationAction.Add || x.Action == ApplicationAction.Edit) && x.Application != null);
 
-        //private async Task<bool> IsDomainUpdated(string applicationDomain, string projectId, CancellationToken _)
-        //{
-        //    var project = await _repository.GetByTenantIdAsync(projectId);
-        //    return project?.ApplicationDomain != applicationDomain;
-        //}
-
-        private async Task<bool> IsUniqueDomain(string applicationDomain, CancellationToken _)
-        {
-            var application = await _repository.GetByDomainAsync(applicationDomain);
-
-            if (application != null)
-            {
-                return false; // If any domain is not unique, return false
-            }
-
-            return true;
+            RuleFor(x => x.ApplicationDomain)
+                .NotEmpty().WithMessage("ApplicationDomain is required for Edit or Delete actions.")
+                .When(x => x.Action == ApplicationAction.Edit || x.Action == ApplicationAction.Delete);
         }
     }
 }
