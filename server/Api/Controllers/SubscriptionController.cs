796921bad078844d082bb0b4e6ec9107858927bd
Koushik Ghosh <koushik.ghosh@selise.ch>
2026-07-02 17:18:09 +0600

    commented out unnecessary code

--- server/Api/Controllers/SubscriptionController.cs
+++ server/Api/Controllers/SubscriptionController.cs
@@ -1,29 +1,29 @@
-﻿using Blocks.Genesis;
-using DomainService.Subscription.RequestModel;
-using DomainService.Subscription.ResponseModel;
-using DomainService.Subscription.Services;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+//using Blocks.Genesis;
+//using DomainService.Subscription.RequestModel;
+//using DomainService.Subscription.ResponseModel;
+//using DomainService.Subscription.Services;
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Mvc;
 
-namespace Api.Controllers
-{
-    [ApiController]
-    [Route("[controller]/[action]")]
+//namespace Api.Controllers
+//{
+//    [ApiController]
+//    [Route("[controller]/[action]")]
 
-    public class SubscriptionController : ControllerBase
-    {
-        private readonly ISubscriptionService _subscriptionService;
+//    public class SubscriptionController : ControllerBase
+//    {
+//        private readonly ISubscriptionService _subscriptionService;
 
-        public SubscriptionController(ISubscriptionService subscriptionService)
-        {
-            _subscriptionService = subscriptionService;
-        }
+//        public SubscriptionController(ISubscriptionService subscriptionService)
+//        {
+//            _subscriptionService = subscriptionService;
+//        }
 
-        [HttpGet]
-        [Authorize]
-        public async Task<GetSubscriptionsResponse> Gets([FromQuery] GetSubscriptionsRequest request)
-        {
-            return await _subscriptionService.GetSubscriptionsAsync(request);
-        }
-    }
-}
+//        [HttpGet]
+//        [Authorize]
+//        public async Task<GetSubscriptionsResponse> Gets([FromQuery] GetSubscriptionsRequest request)
+//        {
+//            return await _subscriptionService.GetSubscriptionsAsync(request);
+//        }
+//    }
+//}
