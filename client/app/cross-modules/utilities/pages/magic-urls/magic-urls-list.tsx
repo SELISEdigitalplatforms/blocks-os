df70fac96677c6c1b6750e721f1e148bf58394fe
afiapreety <afia.preety@selisegroup.com>
2026-07-06 16:58:04 +0600

    fix: empty state nested

--- client/app/cross-modules/utilities/pages/magic-urls/magic-urls-list.tsx
+++ client/app/cross-modules/utilities/pages/magic-urls/magic-urls-list.tsx
@@ -13,7 +13,7 @@ import { Skeleton } from "@/components/ui-kits/skeleton/skeleton";
 import { formatDate, parseDateString } from "@/lib/utils";
 import { IMagicUrlConfig } from "@blocks-utilities/models/magic-url-config.model";
 import { Button } from "@/components/ui-kits/button/button";
-import { EllipsisVertical, Link2, Pencil, Trash } from "lucide-react";
+import { EllipsisVertical, Pencil, Trash } from "lucide-react";
 import { CopyToClipboardButton } from "@/components/copy-to-clipboard-button";
 import { ConfigureMagicUrlModal } from "@blocks-utilities/components/magic-url-config-dialog/configure-magic-url-modal";
 import {
@@ -26,7 +26,6 @@ import { useDeleteMagicUrlConfig } from "@blocks-utilities/hooks/use-magic-url-c
 import ConfirmationModal from "@/components/confirmation-modal/confirmation-modal";
 import { Dialog } from "@/components/ui-kits/dialog/dialog";
 import { showErrorToast, showSuccessToast } from "@/hooks/use-toast";
-import { EmptyState } from "@/components/ui-kits/empty-state";
 
 const LoadingSkelton = () => (
   <div className="grid w-full gap-2">
@@ -148,48 +147,40 @@ export function MagicUrlsList({ configurations, isLoading }: MagicUrlsListProps)
 
   return (
     <>
-      {table.getRowModel().rows?.length ? (
-        <ScrollArea className="w-full">
-          <Table className="text-sm">
-            <TableHeader>
-              {table.getHeaderGroups().map((headerGroup) => (
-                <TableRow key={headerGroup.id} className="px-4 py-2 hover:bg-transparent">
-                  {headerGroup.headers.map((header) => (
-                    <TableHead key={header.id} className="font-bold text-medium-emphasis">
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(header.column.columnDef.header, header.getContext())}
-                    </TableHead>
-                  ))}
-                </TableRow>
-              ))}
-            </TableHeader>
-            <TableBody>
-              {table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                  className="text-medium-emphasis"
-                  isHoverable
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-          <ScrollBar orientation="horizontal" />
-        </ScrollArea>
-      ) : (
-        <EmptyState
-          icon={Link2}
-          title="No configurations found"
-          description="Use Add Configuration to create one."
-        />
-      )}
+      <ScrollArea className="w-full">
+        <Table className="text-sm">
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id} className="px-4 py-2 hover:bg-transparent">
+                {headerGroup.headers.map((header) => (
+                  <TableHead key={header.id} className="font-bold text-medium-emphasis">
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(header.column.columnDef.header, header.getContext())}
+                  </TableHead>
+                ))}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+                className="text-medium-emphasis"
+                isHoverable
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
       {selectedConfig && (
         <ConfigureMagicUrlModal
           configuration={selectedConfig}
