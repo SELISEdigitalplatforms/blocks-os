1d7bf9a9537396655b2cd8dcc831d38f7b0008be
noor-selise <noor.mohammad@selisegroup.com>
2026-07-08 02:58:49 +0600

    feat: add role detail page and update routing

--- client/app/constants/breadcrumb-custom-title.ts
+++ client/app/constants/breadcrumb-custom-title.ts
@@ -1,3 +1,4 @@
 export const BREADCRUMB_CUSTOM_TITLES: Record<string, string | null> = {
   "/app/idp/permission-detail/*": "/app/idp/permissions",
+  "/app/idp/role-detail/*": "/app/idp/roles",
 };
