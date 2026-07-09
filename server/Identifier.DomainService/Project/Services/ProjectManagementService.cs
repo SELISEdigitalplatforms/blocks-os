9e1b02663131ab9711e59b8a8c2db62cb4a8fd13
Koushik Ghosh <koushik.ghosh@selise.ch>
2026-07-09 23:07:32 +0600

    updated

--- server/Identifier.DomainService/Project/Services/ProjectManagementService.cs
+++ server/Identifier.DomainService/Project/Services/ProjectManagementService.cs
@@ -623,7 +623,7 @@ namespace DomainService.Projects
 
             var tokenParams = new
             {
-                IsConfigured = !(project?.ThirdPartyJwtTokenParameters?.PublicCertificatePath == null && project?.ThirdPartyJwtTokenParameters?.JwksUrl == null),
+                IsConfigured = !(string.IsNullOrWhiteSpace(project?.ThirdPartyJwtTokenParameters?.PublicCertificatePath) && string.IsNullOrWhiteSpace(project?.ThirdPartyJwtTokenParameters?.JwksUrl)),
                 ProviderName = project?.ThirdPartyJwtTokenParameters?.ProviderName,
                 Issuer = project?.ThirdPartyJwtTokenParameters?.Issuer,
                 Audiences = project?.ThirdPartyJwtTokenParameters?.Audiences,
