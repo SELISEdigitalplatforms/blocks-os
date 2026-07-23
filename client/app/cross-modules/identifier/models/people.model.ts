ef1d1481fe455c09f65ef5cd5cfca42adbbeaca7
asifrafeen <asif.rafeen@selisegroup.com>
2026-07-24 01:50:26 +0600

    invite url fix

--- client/app/cross-modules/identifier/models/people.model.ts
+++ client/app/cross-modules/identifier/models/people.model.ts
@@ -7,8 +7,6 @@ export interface IPeopleAcceptInvitationPayload {
 export interface IPeopleAcceptInvitationResponse {
   isSuccess: boolean;
   activationKey?: string;
-  /** The project (IAM tenant) the invitation was for, used to build the IAM OIDC activation URL. */
-  tenantId?: string;
   errors?: Record<string, string>;
 }
 
