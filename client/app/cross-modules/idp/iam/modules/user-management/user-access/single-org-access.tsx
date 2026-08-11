65f3dddd80375d5aa30d716757d11428bbf8ba47
afiapreety <afia.preety@selisegroup.com>
2026-08-11 12:24:05 +0600

    fix: permissions resource

--- client/app/cross-modules/idp/iam/modules/user-management/user-access/single-org-access.tsx
+++ client/app/cross-modules/idp/iam/modules/user-management/user-access/single-org-access.tsx
@@ -137,7 +137,7 @@ export const SingleOrgAccess = ({ userId, projectKey }: SingleOrgAccessProps) =>
     try {
       const res = await mutateAsync({
         roles: selectedRolesRef.current.map((role) => role.slug),
-        permissions: selectedPermissionsRef.current.map((permission) => permission.name),
+        permissions: selectedPermissionsRef.current.map((permission) => permission.resource),
         organizationId: DEFAULT_ORG_ID,
       });
       if (!res.isSuccess) {
