2c115ad91c5a3cb00cfcbfc582306409ed1e8030
asifrafeen <asif.rafeen@selisegroup.com>
2026-08-13 22:41:18 +0600

    added release driver in os

--- client/app/cross-modules/devops/constants/endpoint.constant.ts
+++ client/app/cross-modules/devops/constants/endpoint.constant.ts
@@ -2,33 +2,13 @@ import { API_BASES } from "@/constants/endpoint.constant";
 
 export const CLOUD_BUILD_ENDPOINTS = {
   // Authentication & Authorization
-  ACCESS_TOKEN: `${API_BASES.LOGIC}/deployment/accessToken`,
-  IS_AUTHORIZED: `${API_BASES.LOGIC}/deployment/isAuthorized`,
-  REMOVE_AUTHORIZATION: `${API_BASES.LOGIC}/deployment/removeAuthorization`,
-  REMOVE_ACCESS_TOKEN: `${API_BASES.LOGIC}/deployment/removeAccessToken`,
+  ACCESS_TOKEN: `${API_BASES.OS}/release/accessToken`,
+  IS_AUTHORIZED: `${API_BASES.OS}/release/isAuthorized`,
+  REMOVE_AUTHORIZATION: `${API_BASES.OS}/release/removeAuthorization`,
 
   // GitHub Repositories
-  GITHUB_REPOS: `${API_BASES.LOGIC}/deployment/getrepos`,
-  GITHUB_USER: `${API_BASES.LOGIC}/deployment/getuser`,
-  GITHUB_BRANCHES: `${API_BASES.LOGIC}/deployment/getbranches`,
-  GITHUB_BRANCH_EXISTS: `${API_BASES.LOGIC}/deployment/githubbranchExists`,
-
-  // Build & Deployment
-  BUILD_BUILD: "/api/build/clone",
-  RUN_BUILD: "/api/build/run",
-  MANUAL: "/api/build/manual",
-  BUILD: "/api/build",
-
-  // Repository Management
-  REPOS: "/api/repos",
-  REPOS_LIST: "/api/repos/list",
-  REPO_DETAILS: "/api/repos/details",
-
-  // Build Settings
-  SETTINGS: "/api/settings",
-};
-
-export const MIGRATION_ENDPOINTS = {
-  GET_STATUS: "/api/identifier/migration/status",
-  START_MIGRATION: "/api/identifier/migration/start",
+  GITHUB_REPOS: `${API_BASES.OS}/release/getrepos`,
+  GITHUB_USER: `${API_BASES.OS}/release/getuser`,
+  GITHUB_BRANCHES: `${API_BASES.OS}/release/getbranches`,
+  GITHUB_BRANCH_EXISTS: `${API_BASES.OS}/release/githubbranchExists`,
 };
