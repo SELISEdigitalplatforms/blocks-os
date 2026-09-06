a67723dbdcc3c1647bb270f04c2daa6eb02e4722
afiapreety <afia.preety@selisegroup.com>
2026-09-06 17:03:12 +0600

    fix: keep filter as it is

--- client/app/cross-modules/idp/iam/modules/user-management/users/users-filter-toolbar.tsx
+++ client/app/cross-modules/idp/iam/modules/user-management/users/users-filter-toolbar.tsx
@@ -251,12 +251,12 @@ export const UsersDateFilters = () => {
           label: "Last login",
           props: { numberOfMonths: 1 },
         },
-        // {
-        //   key: "lastUpdatedDate",
-        //   type: "DateRange",
-        //   label: "Last updated",
-        //   props: { numberOfMonths: 1 },
-        // },
+        {
+          key: "lastUpdatedDate",
+          type: "DateRange",
+          label: "Last updated",
+          props: { numberOfMonths: 1 },
+        },
       ]}
       values={{
         organizationIds: showOrganizationSelection ? selectedOrganizationIds : [],
